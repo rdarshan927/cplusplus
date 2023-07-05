f6cb941ff2646950acee02209a3ca320f9a40300
Darshan R <64832773+rdarshan927@users.noreply.github.com>
2023-07-05 07:30:24 +0530

    Add files via upload

---
+++ ClassObjectQ/q7.cpp
@@ -0,0 +1,38 @@
+/*
+Write a program to print the area of a rectangle by creating a class named 'Area' taking the values of its length and breadth as parameters of its constructor 
+and having a function named 'returnArea' which returns the area of the rectangle. Length and breadth of the rectangle are entered through keyboard.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+class Area
+{
+    private:
+        int length, breadth;
+
+    public:
+        void getData()
+        {
+            cout<<"Enter the length : ";
+            cin>>length;
+            cout<<"Enter the breadth : ";
+            cin>>breadth;
+        }
+        int returnArea()
+        {
+            return length*breadth;
+        }
+};
+
+int main()
+{
+    Area data;
+
+    data.getData();
+
+    cout<<"Area : "<<data.returnArea()<<endl;
+
+    return 0;
+}
