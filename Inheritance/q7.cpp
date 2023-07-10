a02cf394348a7161f6f15b68c71ec7c362f0ba9b
rdarshan927 <rdarshan927@gmail.com>
2023-07-10 06:26:18 +0530

    incomplete

--- Inheritance/q7.cpp
+++ Inheritance/q7.cpp
@@ -2,29 +2,18 @@
 Write a C++ Program display Student Marksheet using Multiple inheritance. 
 */
 
+//incomplete
+
 #include<iostream>
 
 using namespace std;
-
-class userIntent
-{
-    protected:
-        int user;
-    
-    public:
-        void userInput()
-        {
-            cout<<"1 - Input Details\n2 - Output Details\n3 - Calculate Average\n4 - Exit\nEnter the type : ";
-            cin>>user;
-        }
-};
 
 class studentInput
 {
     protected:
         string stuName;
         int stuYear, sem;
-        float ip, cs, ics, mc, spm, oop, iwt, eng, avg;
+        float ip, cs, ics, mc, spm, oop, iwt, eng;
     
     public:
         void getData()
@@ -65,7 +54,7 @@ class studentInput
         }
 };
 
-class studentOutput: public userIntent, public studentInput
+class studentOutput: public studentInput
 {
     public:
         void display1()
@@ -93,10 +82,70 @@ class studentOutput: public userIntent, public studentInput
         }
 };
 
+class calculation: public studentInput
+{
+    public:
+        void avg()
+        {
+            if (stuYear == 1)
+            {
+                if(sem == 1)
+                {
+                    cout<< (ip+cs+ics+mc)/4.0<<endl;
+                }
+                else if (sem == 2)
+                {
+                    cout<<(spm+oop+iwt+eng)/4.0<<endl;
+                }
+            }
+            //it goes like this ...     
+        }
+};
+
+class exited
+{
+    public:
+        void exiting()
+        {
+            cout<<"Program existed."<<endl;
+        }
+};
+
 
 int main()
 {
+    studentInput data;
+    studentOutput outdata;
+    calculation cal;
+    exited end;
 
+    int user, i;
+
+    for(i=0; i<4; i++)
+    {
+        cout<<"1 - Input Details\n2 - Output Details\n3 - Calculate Average\n4 - Exit\nEnter the type : ";
+        cin>>user;
+
+        switch (user)
+        {
+        case 1:
+            data.getData();
+            break;
+        case 2:
+            outdata.display1();
+            break;
+        case 3:
+            cal.avg();
+            break;
+        case 4:
+            end.exiting();
+            return 0;
+            break;
+        default:
+            cout<<"You have entered an invalid type."<<endl;
+            break;
+        }
+    }
 
     return 0;
 }
